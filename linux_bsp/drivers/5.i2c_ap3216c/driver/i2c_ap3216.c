c9fb04e627283fffb4ecf4ed163f83f1bb9a430b
Xuzhangxin <xzx_work_2020@163.com>
2024-06-13 13:11:28 +0000

    完善i2c_ap3216驱动代码，加入read\write\open等

--- linux_bsp/drivers/5.i2c_ap3216c/driver/i2c_ap3216.c
+++ linux_bsp/drivers/5.i2c_ap3216c/driver/i2c_ap3216.c
@@ -16,6 +16,19 @@
 #include <asm/uaccess.h>
 #include <asm/io.h>
 
+/* AP3316C 寄存器 */
+#define AP3216C_SYSTEMCONG 0x00 /* 配置寄存器 */
+#define AP3216C_INTSTATUS 0X01 /* 中断状态寄存器 */
+#define AP3216C_INTCLEAR 0X02 /* 中断清除寄存器 */
+#define AP3216C_IRDATALOW 0x0A /* IR 数据低字节 */
+#define AP3216C_IRDATAHIGH 0x0B /* IR 数据高字节 */
+#define AP3216C_ALSDATALOW 0x0C /* ALS 数据低字节 */
+#define AP3216C_ALSDATAHIGH 0X0D /* ALS 数据高字节 */
+#define AP3216C_PSDATALOW 0X0E /* PS 数据低字节 */
+#define AP3216C_PSDATAHIGH 0X0F /* PS 数据高字节 */
+
+#define AP3216C_DATA_REG_NUM 6
+
 #define AP3216C_NAME "ap3216c"
 
 struct ap3216c_dev_t {
@@ -26,15 +39,55 @@ struct ap3216c_dev_t {
     struct device_node *nd; ///< 设备节点
     int devid;              ///< 设备号
     int major;               ///< 主设备号
-};
+    struct i2c_client* client; ///<i2c设备
+} ap3216c_dev;
 
 struct ap3216c_dev_t ap3216c_dev = {0}; 
+
+static int ap3216c_write_reg(struct ap3216c_dev *dev, u8 reg, u8 *data, u16 len)
+{
+    int ret = 0;
+    struct i2c_client* client = (i2c_client*)dev->client;
+    struct i2c_msg msg;
+
+    msg.addr = client->addr;            ///< 设备地址
+    msg.flags = 0;                      ///< 写数据
+    msg.buf[0] = reg;                   ///< 操作寄存器地址
+    memcpy(&msg.buf[1], data, len);     ///< 写寄存器的数据
+    msg.len = len + 1 ;                 ///< 1byte寄存器地址+data的长度    
+    return ret = i2c_transfer(client->adapter, &msg, 1);
+}
+
+static int ap3216c_read_reg(struct ap3216c_dev *dev, u8 reg, u8 *data, u16 len)
+{
+    int ret = 0;
+    struct i2c_client* client = (i2c_client*)dev->client;
+    struct i2c_msg msg[2];
+
+    msg[0].addr = client->addr;             ///< 设备地址
+    msg[0].flags = 0;                       ///< 写数据
+    msg[0].buf[0] = reg;                    ///< 操作寄存器地址
+    msg[0].len = 1 ;                        ///< 1byte寄存器地址 
+
+    msg[1].addr = client->addr;            ///< 设备地址
+    msg[1].flags = I2C_M_RD;               ///< 读数据
+    msg[1].buf = data;                     ///< 读取数据缓冲区
+    msg[1].len = len;                      ///< 读取数据长度 
+
+    return ret = i2c_transfer(client->adapter, &msg, 2);
+}
+
 
 static int ap3216c_open(struct inode *node, struct file *file)
 {
     printk("ap3216c driver open\n");
     file->private_data = (void *)&ap3216c_dev; ///< 设置私有数据
     
+    u8 value = 0x00;
+    ap3216c_write_reg(file->private_data, AP3216C_SYSTEMCONG, &value, 1);  ///< set sw reset
+    udelay(20 * 1000); ///< 最少等待10ms
+    value = 0x03;
+    ap3216c_write_reg(file->private_data, AP3216C_SYSTEMCONG, &value, 1);  ///< 设置为ALS and PS+IR模式
 
     printk("ap3216c driver open success\n");
     return 0;
@@ -43,25 +96,31 @@ static int ap3216c_open(struct inode *node, struct file *file)
 static int ap3216c_close(struct inode *node, struct file *file)
 {
     printk("ap3216c driver close\n");
-
+    file->private_data = (void *)&ap3216c_dev; ///< 设置私有数据
+    
+    u8 value = 0x00;
+    ap3216c_write_reg(file->private_data, AP3216C_SYSTEMCONG, &value, 1);  ///< set sw reset
+    
+    printk("ap3216c driver close success\n");
     return 0;
 }
 
 static ssize_t ap3216c_read (struct file *file, char __user *data, size_t cnt, loff_t *off)
 {
+    u8 i = 0;
     int ret = 0;
     printk("ap3216c driver read\n");
 
+    file->private_data = (void *)&ap3216c_dev; ///< 设置私有数据
+
+    u8 buf[AP3216C_DATA_REG_NUM] = {0};
+    /// 循环将IR、ALS、PS中的寄存器数据读取
+    for (i = 0; i < AP3216C_DATA_REG_NUM; i++) {
+        ret = ap3216c_read_reg(file->private_data, AP3216C_IRDATALOW, buf, 6);
+        printk("ap3216c driver read ret:%d, reg:%x, value:%d\n", ret, AP3216C_IRDATALOW + i, buf[i]);
+    }
+
     printk("ap3216c driver read sucess, ret:%d\n", ret);
-    return ret;
-}
-static ssize_t ap3216c_write(struct file *file, const char __user *data, size_t cnt, loff_t *off)
-{
-    int ret = 0;
-    printk("ap3216c driver write\n");
-
-
-    printk("ap3216c driver write sucess\n");
     return ret;
 }
 
@@ -70,7 +129,6 @@ static struct file_operations ap3216c_fops = {
     .open = ap3216c_open,
     .release = ap3216c_close,
     .read = ap3216c_read,
-    .write = ap3216c_write
 };
 
  /* 传统匹配方式 ID 列表 */
